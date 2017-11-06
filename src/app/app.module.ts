8e0da12bafa1ad5f59b7a48eb284d7ac0eef0c49
asuriuma <umadevi.asuri@gmail.com>
2017-11-06 12:03:40 -0500

    Rollback

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
     JavaCompComponent
   ],
   imports: [
-    BrowserModule1,
+    BrowserModule,
     NgbModule.forRoot(),
     RouterModule.forRoot(
       appRoutes
