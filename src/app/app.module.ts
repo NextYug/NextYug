3421fb67117dcf3303413c21ce0c4b54fdc75d3a
asuriuma <umadevi.asuri@gmail.com>
2017-11-06 12:02:08 -0500

    Checking commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
     JavaCompComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule1,
     NgbModule.forRoot(),
     RouterModule.forRoot(
       appRoutes
